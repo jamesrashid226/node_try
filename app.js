fe2b0739fc532dda165c4c640280b9dcdf4f9cde
jamesrashid226 <kali@kali.google>
2024-09-16 21:30:31 -1000

    this is my first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
